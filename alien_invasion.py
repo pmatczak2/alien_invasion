d944cbfeed5b6e7055dc90f63e054637016a7008
piotrmatczak <pmatczak2@gmail.com>
2021-09-25 12:21:03 -0700

    Aliens that reach the bottom of the screen. Creating Game Over! Identifying When Parts of the Game Should Run

--- alien_invasion.py
+++ alien_invasion.py
@@ -132,22 +132,23 @@ class AlienInvasion:
             aline.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1
 
-    def _check_alien_bottom(self):
+    def _check_aliens_bottom(self):
         screen_rect = self.screen.get_rect()
         for alien in self.aliens.sprites():
             if alien.rect.bottom >= screen_rect.bottom:
                 self._ship_hit()
                 break
 
-            self._check_alien_bottom()
+
 
     def _update_aliens(self):
         self._check_fleet_edges()
         self.aliens.update()
-        self._check_alien_bottom()
 
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
+
+        self._check_aliens_bottom()
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)

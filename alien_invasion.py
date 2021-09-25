e5899d2b0ef0e07f7c5441747601491b9090ea0e
piotrmatczak <pmatczak2@gmail.com>
2021-09-25 12:15:45 -0700

    Aliens that reach the bottom of the screen. Creating Game Over! Identifying When Parts of the Game Should Run

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,9 +31,12 @@ class AlienInvasion:
     def run_game(self):
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
     def _check_events(self):

7c7c4261f881af08205fa88d9b1f30d587b47316
piotrmatczak <pmatczak2@gmail.com>
2021-09-28 13:47:00 -0700

    starting the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -49,6 +49,13 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
 
     def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:

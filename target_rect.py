19a94a5fae118adbc75a3ef0a9cf4ee5e3501362
piotrmatczak <pmatczak2@gmail.com>
2021-09-29 11:21:09 -0700

    creating target

--- target_rect.py
+++ target_rect.py
@@ -14,3 +14,13 @@ class Target:
         self.rect.topright = self.screen.get_rect
 
         self._prep_msg(msg)
+
+    def _prep_msg(self, msg):
+        self.msg_image = self.font.render(msg, True, self.text_color,
+                self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.topright = self.rect.topright
+
+    def draw_target(self):
+        self.screen.fill(self.target_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)

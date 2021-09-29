1bb9d98ebd7a9766df4270c5510560bf196cc1a5
piotrmatczak <pmatczak2@gmail.com>
2021-09-28 17:05:13 -0700

    press P to play

--- alien_invasion.py
+++ alien_invasion.py
@@ -57,15 +57,18 @@ class AlienInvasion:
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
             self.stats.reset_stats()
-            self.stats.game_active = True
+            self._start_game()
 
-            pygame.mouse.set_visible(False)
+    def _start_game(self):
+        self.stats.game_active = True
 
-            self.aliens.empty()
-            self.bullets.empty()
+        pygame.mouse.set_visible(False)
 
-            self._create_fleet()
-            self.ship.center_ship()
+        self.aliens.empty()
+        self.bullets.empty()
+
+        self._create_fleet()
+        self.ship.center_ship()
 
     def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -76,6 +79,9 @@ class AlienInvasion:
             sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
+        elif event.key == pygame.K_p and not self.stats.game_active:
+            self._start_game()
+
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:

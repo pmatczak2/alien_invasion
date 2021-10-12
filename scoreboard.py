22afc8b29504b5deaf6cff0dbfe58d697ed89de8
piotrmatczak <pmatczak2@gmail.com>
2021-10-12 04:34:00 -0700

    created a scoreboard class

---
+++ scoreboard.py
@@ -0,0 +1,14 @@
+import pygame.font
+
+class Scoreboard:
+
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+        self.settings = ai_game.settings
+        self.stat = ai_game.stats
+
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+
+        self.prep_score()

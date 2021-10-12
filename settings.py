2c00d546b8a4f79e8759d9e1a5bbae5378126a0b
piotrmatczak <pmatczak2@gmail.com>
2021-10-11 19:16:22 -0700

    creating 3 difficulty levels and 3 difficulty buttons

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ class Settings:
         self.ship_limit = 3
 
         self.bullet_speed = 1.5
-        self.bullet_width = 3000
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
@@ -19,12 +19,29 @@ class Settings:
 
         self.speedup_scale = 1.1
 
+        self.difficulty_level = 'medium'
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
-        self.ship_speed = 1.5
-        self.bullet_speed = 3.0
-        self.alien_speed = 1.0
+        if self.difficulty_level == 'easy':
+            self.ship_limit = 5
+            self.bullets_allowed = 10
+            self.ship_speed = 0.75
+            self.bullet_speed = 1.5
+            self.alien_speed = 0.5
+        elif self.difficulty_level == 'medium':
+            self.ship_limit = 3
+            self.bullets_allowed = 3
+            self.ship_speed = 1.5
+            self.bullet_speed = 3.0
+            self.alien_speed = 1.0
+        elif self.difficulty_level == 'difficult':
+            self.ship_limit = 2
+            self.bullets_allowed = 3
+            self.ship_speed = 3.0
+            self.bullet_speed = 6.0
+            self.alien_speed = 2.0
 
         self.fleet_direction = 1
 

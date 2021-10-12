ac98c876c5b6aa03bb37850c05a843251871bbac
piotrmatczak <pmatczak2@gmail.com>
2021-10-12 04:28:44 -0700

    created a scoreboard

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
-        self.ship_limit = 3
+        self.ship_limit = 5
 
         self.bullet_speed = 1.5
         self.bullet_width = 3
@@ -13,8 +13,8 @@ class Settings:
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
 
-        self.alien_speed = 1.0
-        self.fleet_drop_speed = 50
+        self.alien_speed = 0.5
+        self.fleet_drop_speed = 5
         self.fleet_direction = 1
 
         self.speedup_scale = 1.1

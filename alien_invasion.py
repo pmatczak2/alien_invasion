edb3bb90332bba45bf2587087e7fd43e1b9ce532
piotrmatczak <pmatczak2@gmail.com>
2021-10-12 17:09:57 -0700

    resetting the score

--- alien_invasion.py
+++ alien_invasion.py
@@ -60,7 +60,19 @@ class AlienInvasion:
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
             self.settings.initialize_dynamic_settings()
-            self._start_game()
+            self.stats.reset_stats()
+            self.stats.game_active = True
+            self.sb.prep_score()
+
+            self.aliens.empty()
+            self.bullets.empty()
+
+
+            self._create_fleet()
+            self.ship.center_ship()
+
+
+            pygame.mouse.set_visible(False)
 
     def _start_game(self):
         self.stats.game_active = True
